e6330ba8c7ed92de8953b17c99e3f5548b3557fe
Dang Thach <nvanhaidang@gmail.com>
2021-03-03 20:40:37 +0700

    Su khac biet giua var va let

--- Buoi1.js
+++ Buoi1.js
@@ -1,8 +1,24 @@
 // Command + Shift + . : Hien thi thu muc an
 
-# git status
-# git add . : Luu tru tat ca cac thay doi
-# git commit -m <message>: Luu ghi chu cho lan thay doi
+// # git status
+// # git add . : Luu tru tat ca cac thay doi
+// # git commit -m <message>: Luu ghi chu cho lan thay doi
 
 // Bo qua files folder
 // Tao file .gitignore
+
+// re-create : Bi khoi tao lai
+// let a = 11;
+// let a = 10;
+
+// scope ({}) : Pham vi
+// var khong chac che trong scope
+
+// if (true) {
+//   var a = 10;
+// }
+
+// Co che hoisting: khi dung var viec khai bao se dat truoc
+// kiem tra gia tri
+console.log(a);
+var a = 10;
