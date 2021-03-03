14fa3033b72498fcb824f91e882cb1ef6b72a330
Dang Thach <nvanhaidang@gmail.com>
2021-03-03 20:20:38 +0700

    Add gitinore

---
+++ Buoi1.js
@@ -0,0 +1,8 @@
+// Command + Shift + . : Hien thi thu muc an
+
+# git status
+# git add . : Luu tru tat ca cac thay doi
+# git commit -m <message>: Luu ghi chu cho lan thay doi
+
+// Bo qua files folder
+// Tao file .gitignore
